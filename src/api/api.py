b392d944862e4f1c1082170a65bf69b146bb9969
Joffrey <joffreybvn@gmail.com>
2020-12-02 09:44:46 +0100

    https fix

--- src/api/api.py
+++ src/api/api.py
@@ -1,5 +1,6 @@
 
 from flask import Flask
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 # App blueprint imports
 from src.api.v1 import blueprint as blueprint_v1
@@ -9,6 +10,9 @@ def create_api():
 
     # Initialize the Flask server
     flask = Flask(__name__)
+
+    # Fix the Swagger UI https
+    flask.wsgi_app = ProxyFix(flask.wsgi_app, x_proto=1, x_host=1)
 
     # Load the blueprints
     flask.register_blueprint(blueprint_v1, url_prefix='/v1')
